f51aa458a1d882146b917ba8ff1facb73f04806e
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-03-25 11:02:25 -0300

    create models

---
+++ src/db/models/Message.entity.ts
@@ -0,0 +1,44 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from './User.entity';
+
+@ObjectType()
+@Entity({ name: 'messages' })
+export class Message {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column({ name: 'user_id' })
+  userId: number;
+
+  @Field()
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @Field()
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @Field(() => User)
+  user: User;
+
+  // Associations
+
+  @ManyToOne(
+    () => User,
+    user => user.messageConnection,
+    { primary: true },
+  )
+  @JoinColumn({ name: 'user_id' })
+  userConnection: Promise<User>;
+}
---
+++ src/db/models/User.entity.ts
@@ -0,0 +1,36 @@
+import { Field } from '@nestjs/graphql';
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Message } from './Message.entity';
+
+@Entity()
+export class User {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  email: string;
+
+  @Field()
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @Field()
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  //Associations
+  @OneToMany(
+    () => Message,
+    message => message.userConnection,
+  )
+  messageConnection: Promise<Message[]>;
+}
