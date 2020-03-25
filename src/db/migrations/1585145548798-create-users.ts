ba4513edb07faa6ee784dc6540c24c5b9ad4b253
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-03-25 12:10:25 -0300

    migrations

---
+++ src/db/migrations/1585145548798-create-users.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createUsers1585145548798 implements MigrationInterface {
+  private table = new Table({
+    name: 'users',
+    columns: [
+      {
+        name: 'id',
+        type: 'integer',
+        isPrimary: true,
+        isGenerated: true,
+        generationStrategy: 'increment',
+      },
+      {
+        name: 'email',
+        type: 'varchar',
+        length: '255',
+        isUnique: true,
+        isNullable: false,
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+      {
+        name: 'created_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(this.table);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable(this.table);
+  }
+}
